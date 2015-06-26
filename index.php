fb624cd6422c9d0e24caef5051fc27fcdebd6139
Valchovski <bvalchovski@abv.bg>
2015-06-26 14:56:36 +0300

    Index Page
    Adding a simple login page that has to be graphically modified, when
    done with authorize.php technically it SHOULD work.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Wrecking HackTUES</title>
+    <link rel="Icon" href="title_logo.png" />
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	
+	<script>
+	</script>
+	
+</head>
+
+<body>
+	<div class="container">
+		<div class="LoginPage">
+			<fieldset style="width:30%">
+				<h3>Вход за учители</h3>
+				<form method="POST" action="authorize.php"> 
+					User <br><input type="text" name="user" size="40" autocomplete="off"/><br> 
+					Password <br><input type="password" name="pass" size="40" autocomplete="off" /><br> 
+					<input id="button" type="submit" name="submit" value="Log-In"/> 
+				</form> 
+			</fieldset>
+		</div>
+        </div>
+</body>
+
+</html>
